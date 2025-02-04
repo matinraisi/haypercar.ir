a8e2ece815aaecf7ff2f96d8402aeee3fe7ca8fa
matinraisi <raisi.m630@gmail.com>
2025-02-04 14:09:45 +0330

    add page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request , 'core/home.html')
+
+def shopping_cart(request):
+    return render(request , 'core/shopping_cart.html')
