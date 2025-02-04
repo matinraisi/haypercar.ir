72940b9e4269d2e874f5c5ff578d2f28a8290f7a
matinraisi <raisi.m630@gmail.com>
2025-02-04 15:04:16 +0330

    add pages dashboard

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('shopping_cart', views.shopping_cart, name='shopping_cart'),
+    path('account_settlement', views.account_settlement, name='account_settlement'),
+    path('Product_details', views.Product_details, name='Product_details'),
+    path('account_dashboard', views.account_dashboard, name='account_dashboard'),
+    path('account_profile', views.account_profile, name='account_profile'),
+    path('account_orders', views.account_orders, name='account_orders'),
+    path('account_order_details', views.account_order_details, name='account_order_details'),
+    path('account_addresses', views.account_addresses, name='account_addresses'),
+    path('account_edit_address', views.account_edit_address, name='account_edit_address'),
+    path('account_password', views.account_password, name='account_password'),
+
+
 
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,28 @@ def home(request):
 
 def shopping_cart(request):
     return render(request , 'core/shopping_cart.html')
+
+def account_settlement(request):
+    return render(request , 'core/account_settlement.html')
+
+def Product_details(request):
+    return render(request , 'core/Product_details.html')
+
+def account_dashboard(request):
+    return render(request , 'core/account-dashboard.html')
+def account_profile(request):
+    return render(request , 'core/account-profile.html')
+def account_orders(request):
+    return render(request , 'core/account-orders.html')
+
+def account_order_details(request):
+    return render(request , 'core/account-order-details.html')
+
+def account_addresses(request):
+    return render(request , 'core/account-addresses.html')
+def account_edit_address(request):
+    return render(request , 'core/account-edit-address.html')
+def account_password(request):
+    return render(request , 'core/account-password.html')
+
+
